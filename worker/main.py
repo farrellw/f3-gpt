07abd9f321036cda8942acc85dd3b6883f93ca41
Will Farrell <willfarrell@TP-LT-0106.attlocal.net>
2023-08-01 11:12:04 -0500

    Add langchain implementation.

--- worker/main.py
+++ worker/main.py
@@ -9,6 +9,11 @@ import openai
 import mysql.connector
 import json
 
+from langchain.llms import OpenAI
+from langchain.utilities import SQLDatabase
+from langchain_experimental.sql import SQLDatabaseChain
+from langchain.prompts.prompt import PromptTemplate
+
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
@@ -22,7 +27,7 @@ class DecimalEncoder(json.JSONEncoder):
   
 
 def generate_prompt_1(text): 
-   prompt = """You are a ChatGPT language model that can generate SQL queries. Please provide a natural language input text, and I will generate the corresponding SQL query for you. There is one view to query called attendance_view. The view itself is a record of attendance at workouts for an organization called "F3". Each time a user attends a workout, there is an entry in the table.It has columns Date, AO, PAX, Q. I'll define each column here.PAX is another name for user. So if a user was called "Catalina", then "Catalina" would be in the PAX column.Date is the date that the user/PAX attended the workout.AO is the location of the workout ( The current locations are blackops, ao_backyard_tower_grove, ao_bunker_lindenwood_park, ao_battery_lafayette_park, ao_badlands_francis_park, ao_bear_pit_carondelet_park, qsource, rucking, ao_southside_shuffle_tilles_park, ao_brickyard_turtle_park, c25k. More locations may be added later, but for example if someone says bear pit they mean ao_bear_pit_carondelet_park ). Q is the user who led the workout.
+   prompt = """You are a ChatGPT language model that can generate SQL queries. Please provide a natural language input text, and I will generate the corresponding SQL query for you. There is one view to query called attendance_view. The view itself is a record of attendance at workouts for an organization called "F3". Each time a user attends a workout, there is an entry in the table.It has columns Date, AO, PAX, Q. I'll define each column here.PAX is another name for user. So if a user was called "Catalina", then "Catalina" would be in the PAX column.Date is the date that the user/PAX attended the workout.AO is the location of the workout ( The current locations are blackops, ao_backyard_tower_grove, ao_bunker_lindenwood_park, ao_battery_lafayette_park, ao_badlands_francis_park, ao_bear_pit_carondelet_park, qsource, rucking, ao_southside_shuffle_tilles_park, ao_brickyard_turtle_park, c25k. More locations may be added later, but for example if someone says bear pit they mean ao_bear_pit_carondelet_park ). Q is the user who led the workout. Because the table is attendance, if ten PAX attended a workout on the same day, the Q would be the same for all ten of these user entries. However it was a single workout, so when counting times a user led a workout take the concatenation of date and Q.
     \nInput: {}\nSQL Query:""".format(text)
    return prompt
 
@@ -51,6 +56,32 @@ def execute_sql_query(cursor, query):
     cursor.execute(query)
     result = cursor.fetchall()
     return result
+
+def langchang_implementation(input):
+    db = SQLDatabase.from_uri(os.getenv("FULL_CONNECTION_URI"))
+    llm = OpenAI(temperature=0, verbose=True)
+   
+    _DEFAULT_TEMPLATE = """Given an input question, first create a syntactically correct msyql query to run, then look at the results of the query and return the answer.
+    Use the following format:
+
+    Question: "Question here"
+    SQLQuery: "SQL Query to run"
+    SQLResult: "Result of the SQLQuery"
+    Answer: "Final answer here"
+
+    There is one view to query called attendance_view. The view itself is a record of attendance at workouts for an organization called "F3". Each time a user attends a workout, there is an entry in the table.It has columns Date, AO, PAX, Q. I'll define each column here.PAX is another name for user. So if a user was called "Catalina", then "Catalina" would be in the PAX column.Date is the date that the user/PAX attended the workout.AO is the location of the workout ( The current locations are blackops, ao_backyard_tower_grove, ao_bunker_lindenwood_park, ao_battery_lafayette_park, ao_badlands_francis_park, ao_bear_pit_carondelet_park, qsource, rucking, ao_southside_shuffle_tilles_park, ao_brickyard_turtle_park, c25k. More locations may be added later, but for example if someone says bear pit they mean ao_bear_pit_carondelet_park ). Q is the user who led the workout. Because the table is attendance, if ten PAX attended a workout on the same day, the Q would be the same for all ten of these user entries. However it was a single workout, so when counting times a user led a workout take the concatenation of date and Q.
+
+    Question: {input}"""
+    
+    PROMPT = PromptTemplate(
+        input_variables=["input"], template=_DEFAULT_TEMPLATE
+    )
+    
+    db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True, prompt = PROMPT, use_query_checker=True)
+    
+    r = db_chain.run(input)
+
+    return r
 
 # Triggered from a message on a Cloud Pub/Sub topic.
 @functions_framework.cloud_event
@@ -104,7 +135,6 @@ def worker(cloud_event):
 
         try:
             result_as_json = json.dumps(result)
-            
         except Exception as e:
             result_as_json = json.dumps(result, cls=DecimalEncoder)
 
@@ -114,6 +144,7 @@ def worker(cloud_event):
             text=f'DB Response: {result_as_json}',
             thread_ts=ts
         )
+
         rrr = generate_prompt_2(prompt_1, f3_gpt_response, result_as_json)
 
         request = openai.ChatCompletion.create(
@@ -124,6 +155,13 @@ def worker(cloud_event):
         
         human_readable = request['choices'][0]['message']['content']
     
+        response = client.chat_postMessage(
+            channel=channel_id,
+            text=human_readable,
+            thread_ts=ts
+        )
+    elif "[langchain]" in request_text.lower():
+        human_readable = langchang_implementation(request_text)
         response = client.chat_postMessage(
             channel=channel_id,
             text=human_readable,
